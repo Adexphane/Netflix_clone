4ef053eb350fc69041102bf36eed7d42dddfd574
Adexphane <adexphane@gmail.com>
2025-02-05 19:43:35 +0100

    auth_route_done

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,59 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+import prismadb from "@/lib/prismadb";
+import { compare } from "bcryptjs";
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+export default NextAuth({
+  providers: [
+    Credentials({
+      id: "credentials",
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials: SignInCredentials | undefined) => {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password required");
+        }
+
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user.hashedPassword) {
+          throw new Error("Email does not exist");
+        }
+
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error(`Incorrect Password`);
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
